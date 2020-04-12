3f64e26e9f0aafda909e44eadaa2f4c3a58d2769
RaghuManne09 <38254311+RaghuManne09@users.noreply.github.com>
2020-04-12 16:13:14 +0530

    Add files via upload

--- login.php
+++ login.php
@@ -1,53 +1,49 @@
 <?php
-    include_once 'database.php';
-    session_start();
-    if(isset($_SESSION["email"]))
+	require('database.php');
+	session_start();
+	if(isset($_SESSION["email"]))
 	{
 		session_destroy();
-    }
-    
-    $ref=@$_GET['q'];
-    if(isset($_POST['submit']))
+	}
+	
+	$ref=@$_GET['q'];		
+	if(isset($_POST['submit']))
 	{	
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-
-        $email = stripslashes($email);
-        $email = addslashes($email);
-        $password = stripslashes($password); 
-        $password = addslashes($password);
-
-        $email = mysqli_real_escape_string($con,$email);
-        $password = mysqli_real_escape_string($con,$password);
-        
-        $result = mysqli_query($con,"SELECT email FROM admin WHERE email = '$email' and password = '$password'") or die('Error');
-        $count=mysqli_num_rows($result);
-        if($count==1)
-        {
-            session_start();
-            if(isset($_SESSION['email']))
-            {
-                session_unset();
-            }
-            $_SESSION["name"] = 'Admin';
-            $_SESSION["key"] ='admin';
-            $_SESSION["email"] = $email;
-            header("location:dashboard.php?q=0");
-        }
-        else
-        {
-            echo "<center><h3><script>alert('Sorry.. Wrong Username (or) Password');</script></h3></center>";
-            header("refresh:0;url=admin.php");
-        }
-    }
+		$email = $_POST['email'];
+		$pass = $_POST['password'];
+		$email = stripslashes($email);
+		$email = addslashes($email);
+		$pass = stripslashes($pass); 
+		$pass = addslashes($pass);
+		$email = mysqli_real_escape_string($con,$email);
+		$pass = mysqli_real_escape_string($con,$pass);					
+		$str = "SELECT * FROM user WHERE email='$email' and password='$pass'";
+		$result = mysqli_query($con,$str);
+		if((mysqli_num_rows($result))!=1) 
+		{
+			echo "<center><h3><script>alert('Sorry.. Wrong Username (or) Password');</script></h3></center>";
+			header("refresh:0;url=login.php");
+		}
+		else
+		{
+			$_SESSION['logged']=$email;
+			$row=mysqli_fetch_array($result);
+			$_SESSION['name']=$row[1];
+			$_SESSION['id']=$row[0];
+			$_SESSION['email']=$row[2];
+			$_SESSION['password']=$row[3];
+			header('location: welcome.php?q=1'); 					
+		}
+	}
 ?>
+
 <!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
-		<title>Admin Login | Online Quiz System</title>
+		<title>Login | Online Quiz System</title>
 		<link rel="stylesheet" href="scripts/bootstrap/bootstrap.min.css">
 		<link rel="stylesheet" href="scripts/ionicons/css/ionicons.min.css">
 		<link rel="stylesheet" href="css/form.css">
@@ -69,8 +65,8 @@
 				<div class="box-wrapper">				
 					<div class="box box-border">
 						<div class="box-body">
-						<center> <h5 style="font-family: Noto Sans;">Login to </h5><h4 style="font-family: Noto Sans;">Admin Page</h4></center><br>
-							<form method="post" action="admin.php" enctype="multipart/form-data">
+						<center> <h5 style="font-family: Noto Sans;">Login to </h5><h4 style="font-family: Noto Sans;">Online Quiz System</h4></center><br>
+							<form method="post" action="login.php" enctype="multipart/form-data">
 								<div class="form-group">
 									<label>Enter Your Email Id:</label>
 									<input type="email" name="email" class="form-control">
@@ -84,7 +80,9 @@
 								<div class="form-group text-right">
 									<button class="btn btn-primary btn-block" name="submit">Login</button>
 								</div>
-								
+								<div class="form-group text-center">
+									<span class="text-muted">Don't have an account?</span> <a href="register.php">Register</a> Here..
+								</div>
 							</form>
 						</div>
 					</div>
